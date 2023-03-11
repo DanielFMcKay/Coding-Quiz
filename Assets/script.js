4d1218ebe3b797d7838d00f6a8a7824e4047b40f
Dan <123746582+DanielFMcKay@users.noreply.github.com>
2023-03-11 01:10:30 -0800

    finally made storage and display work properly

--- Assets/script.js
+++ Assets/script.js
@@ -22,6 +22,7 @@ initialsField.style.visibility = 'hidden';
 const finalBtns = document.getElementById("final-buttons");
 finalBtns.style.visibility = 'hidden';
 const scoresList = document.getElementById("scores-list");
+scoresList.style.visibility = 'hidden';
 const homePageBtn = document.getElementById("home-page-btn");
 const clearScores = document.getElementById("clear-scores-btn");
 
@@ -32,14 +33,14 @@ const startButton = document.getElementById("start-button");
 const quizQuestion = [
   {
     currentQuestion: "var xVariable: '0' is an example of what type of variable?",
-  // The choices are in an array.
+    // The choices are in an array.
     choices: ["boolean", "string", "number", "undefined"],
-  // Each question has one objectively correct answer.
+    // Each question has one objectively correct answer.
     rightAnswer: 1
   },
   {
     currentQuestion: "If the four buttons below are coded as an array of four elements, please click the button that corresponds to index 2 in the array.",
-    choices: ["2","let variable = [2,2,2,2]", "object", "This is a trick question, read it carefully. WAIT THE TIMER IS ALM-"],
+    choices: ["2", "let variable = [2,2,2,2]", "object", "This is a trick question, read it carefully. WAIT THE TIMER IS ALM-"],
     rightAnswer: 2
   },
   {
@@ -71,16 +72,16 @@ var finalScore;
 // declares the timer, which is inside the quiz field and thus only starts when the quiz does
 function timerStart() {
   // Sets interval in variable
-  let timerInterval = setInterval(function() {
+  let timerInterval = setInterval(function () {
     secondsLeft--;
-// displays time left
+    // displays time left
     timeLeft.textContent = secondsLeft + " seconds remaining.";
 
-    if(secondsLeft <= 0) {
-     clearInterval(timerInterval);
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
       return timesUp();
     }
-// Declares that each interval is exactly 1000 milliseconds, or 1 second.
+    // Declares that each interval is exactly 1000 milliseconds, or 1 second.
   }, 1000);
 }
 
@@ -91,8 +92,8 @@ let currentQuiz;
 function quizStart() {
   questNumber = -1;
   timerStart();
-  document.getElementById("start-page").style.display="none";
-  document.getElementById("quiz-field").style.display="visible";
+  document.getElementById("start-page").style.display = "none";
+  document.getElementById("quiz-field").style.display = "visible";
   displayScore.style.visibility = 'visible';
   showQuestion(quizQuestion);
 }
@@ -106,92 +107,92 @@ function showQuestion() {
     return quizEnd();
   } else {
 
-  console.log(`current question is number ${questNumber}`);
-  console.log(`The current score is ${currentScore}`);
+    console.log(`current question is number ${questNumber}`);
+    console.log(`The current score is ${currentScore}`);
 
 
-  let currentQuiz = quizQuestion[questNumber];
+    let currentQuiz = quizQuestion[questNumber];
 
-  var questionField = document.createElement("h1");
-  questionField.setAttribute("id", "question-field");
-  var buttonField = document.createElement("ul");
-  buttonField.setAttribute("id", "button-field");
-  buttonField.addEventListener("click", function (q) {
+    var questionField = document.createElement("h1");
+    questionField.setAttribute("id", "question-field");
+    var buttonField = document.createElement("ul");
+    buttonField.setAttribute("id", "button-field");
+    buttonField.addEventListener("click", function (q) {
       verify(q);
-  });
+    });
 
-  // declares the different buttons corresponding to the quiz choices
-  var ch1 = document.createElement('li');
-  ch1.setAttribute("id", "btn-one");
-  var ch2 = document.createElement('li');
-  ch2.setAttribute("id", "btn-two");
-  var ch3 = document.createElement('li');
-  ch3.setAttribute("id", "btn-three");
-  var ch4 = document.createElement('li');
-  ch4.setAttribute("id", "btn-four");
+    // declares the different buttons corresponding to the quiz choices
+    var ch1 = document.createElement('li');
+    ch1.setAttribute("id", "btn-one");
+    var ch2 = document.createElement('li');
+    ch2.setAttribute("id", "btn-two");
+    var ch3 = document.createElement('li');
+    ch3.setAttribute("id", "btn-three");
+    var ch4 = document.createElement('li');
+    ch4.setAttribute("id", "btn-four");
 
 
-  questionField.textContent = currentQuiz.currentQuestion;
-  ch1.textContent = currentQuiz.choices[0];
-  ch2.textContent = currentQuiz.choices[1];
-  ch3.textContent = currentQuiz.choices[2];
-  ch4.textContent = currentQuiz.choices[3];
+    questionField.textContent = currentQuiz.currentQuestion;
+    ch1.textContent = currentQuiz.choices[0];
+    ch2.textContent = currentQuiz.choices[1];
+    ch3.textContent = currentQuiz.choices[2];
+    ch4.textContent = currentQuiz.choices[3];
 
-  // wipes the quiz area after each question
-  quizField.innerHTML = "";
-  quizField.append(questionField);
-  // deploys the response buttons and links them to what they do
-  questionField.append(buttonField);
-  buttonField.append(ch1);
-  buttonField.append(ch2);
-  buttonField.append(ch3);
-  buttonField.append(ch4);
+    // wipes the quiz area after each question
+    quizField.innerHTML = "";
+    quizField.append(questionField);
+    // deploys the response buttons and links them to what they do
+    questionField.append(buttonField);
+    buttonField.append(ch1);
+    buttonField.append(ch2);
+    buttonField.append(ch3);
+    buttonField.append(ch4);
 
-  displayScore.innerHTML = `Your current score is: ${currentScore}.`;
-}
+    displayScore.innerHTML = `Your current score is: ${currentScore}.`;
+  }
 
-function verify(q) {
+  function verify(q) {
 
-  let currentQuiz = quizQuestion[questNumber];
-  let rightAnswersCurrent = currentQuiz.rightAnswer;
-  let rightAnswerText = currentQuiz.choices[rightAnswersCurrent];
-  
-  console.log(`the right answer is ${rightAnswerText}.`);
-  
-  // tells the user if they got a question right or not before moving on to the next question
-  let splashResult = document.createElement("h1");  
-  var quizField = document.getElementById("quiz-field");
-  quizField.appendChild(splashResult);
-  if (q.target.textContent === rightAnswerText) {
+    let currentQuiz = quizQuestion[questNumber];
+    let rightAnswersCurrent = currentQuiz.rightAnswer;
+    let rightAnswerText = currentQuiz.choices[rightAnswersCurrent];
+
+    console.log(`the right answer is ${rightAnswerText}.`);
+
+    // tells the user if they got a question right or not before moving on to the next question
+    let splashResult = document.createElement("h1");
+    var quizField = document.getElementById("quiz-field");
+    quizField.appendChild(splashResult);
+    if (q.target.textContent === rightAnswerText) {
       currentScore++;
       splashResult.textContent = "GIT R DUN!! (correct)";
       secondsLeft = secondsLeft + 3;
-  } else {
+    } else {
       splashResult.textContent = "Dagnammit!! (wrong)";
       secondsLeft = secondsLeft - 9;
+    }
+    // closes the splash feedback and moves to the next question after 1 second
+    setTimeout(showQuestion, 1000);
   }
-  // closes the splash feedback and moves to the next question after 1 second
-  setTimeout(showQuestion, 1000);
-}
 }
 
 
 
 // quiz end if the score ends due to time being up (includling if you drop below zero by getting the last question wrong)
 function timesUp() {
-  quizResult.style.visibility='visible';
-  quizField.style.display='none';
+  quizResult.style.visibility = 'visible';
+  quizField.style.display = 'none';
   quizResult.innerHTML = `You answered ${currentScore} questions correctly. Also, Time's Up!`;
-  displayScore.style.display='none';
-  secondsLeft===undefined;
+  displayScore.style.display = 'none';
+  secondsLeft === undefined;
   // completely hides the timer
-  document.getElementById("timer").style.display="none";
+  document.getElementById("timer").style.display = "none";
   finalScore = currentScore;
   // reveals all the highscore elements
   enterInitials.style.visibility = 'visible';
   initialsBtn.style.visibility = 'visible';
   initialsField.style.visibility = 'visible';
-  highscores.style.visibility="visible";
+  scoresList.style.visibility = 'visible';
 }
 
 
@@ -200,19 +201,19 @@ function timesUp() {
 
 // quiz end if you finish the last question without running out of time
 function quizEnd() {
-  quizResult.style.visibility='visible';
-  quizField.style.display='none';
+  quizResult.style.visibility = 'visible';
+  quizField.style.display = 'none';
   quizResult.innerHTML = `You answered ${currentScore} questions correctly.`;
-  displayScore.style.display='none';
+  displayScore.style.display = 'none';
   finalScore = currentScore;
-  document.getElementById("timer").style.display="none";
+  document.getElementById("timer").style.display = "none";
   // bypasses the timer's countdown to prevent the "Time's Up!" trigger
-  secondsLeft=undefined;
-// if I were to do all these same reveals in more than two different conditions, I'd just make it its own function probably. But it's just twice.
+  secondsLeft = undefined;
+  // if I were to do all these same reveals in more than two different conditions, I'd just make it its own function probably. But it's just twice.
   enterInitials.style.visibility = 'visible';
   initialsBtn.style.visibility = 'visible';
   initialsField.style.visibility = 'visible';
-  highScores.style.visibility="visible";
+  scoresList.style.visibility = 'visible';
 }
 
 
@@ -220,50 +221,76 @@ function quizEnd() {
 
 // function to display scores. Still working on it.
 function renderLatestScores() {
-  // for (var i = 0; i < highScoresList.length; i++) {
-  //   var playerEntry = highScoresList[i];
-  //   console.log(`PlayerEntry variable is ${playerEntry}`);
+
+  // grabs the scores in storage, still in array form
+  var highScoresList = JSON.parse(localStorage.getItem('scores'));
+  console.log(highScoresList);
+  console.log("is highScoresList on retrieval");
+
+  // makes the Recent Scores title visible (formerly "High Scores")
+  highScores.style.visibility = 'visible';
+  // starts a for loop going through the array, and grabbing each initial and score by position
+  for (var i = 0; i < highScoresList.length; i++) {
     var entry = document.createElement('li');
+    // converts initials and score into variables during the course of the for loop
+    var listInit = highScoresList[i].initials;
+    var listScore = highScoresList[i].score;
+
+    console.log("listInit is");
+    console.log(listInit);
+    console.log("listScore is")
+    console.log(listScore);
     // li.textContent = playerEntry;
-    entry.textContent = `${localStorage.getItem('initials')}:  ${localStorage.getItem('final score')}`;
-    console.log(`li.textcontent is ${entry.textcontent}`)
+    // entry.textContent = `${entryDisplay}:  ${localStorage.getItem('final score')}`;
+
+    // creates a text display each initial and score combination to display in order
+    entry.textContent = `${listInit}:  ${listScore}`;
+
+    console.log("entry.textcontent is");
+    console.log(entry.textcontent);
     entry.setAttribute('id', 'scores-list');
-    scoresList.append(entry);
-  } console.log(`errorfieldrender`);
-
-
-
-
-function quizDataInit() {
-  var storedScores = JSON.parse(localStorage.getItem('scores'));
-  if (storedScores !== null) {
-    highScoresList = storedScores;
-  renderLatestScores();
-  console.log(`errorfield1`);
+    // adds the text, and by prepending it displays from newest to oldest
+    scoresList.prepend(entry);
+  } console.log(`scores should render`);
 }
-}
-// I have to declare this twice or for some reason the program breaks
-var initialsStored = initialsField.value.trim();
 
 
-var highScoresList = [initialsStored, finalScore];
+
+
+
 
 function storeScores() {
+  var highScoresList = JSON.parse(localStorage.getItem('scores')) || [];
+  console.log(highScoresList);
+  console.log("is highScoresList on storage");
+
+  var initialsStored = initialsField.value.trim();
+// stores initials and scores as pairs added to an array
+  var scoreEntry = {
+    initials: initialsStored,
+    score: finalScore
+  }
+  console.log(scoreEntry);
+  console.log("is ScoreEntry");
+  // adds new initials and score into highScoresList array
+  highScoresList.push(scoreEntry);
   localStorage.setItem('scores', JSON.stringify(highScoresList));
 }
 
 
-  initialsBtn.addEventListener("click", function (event) {
+
+// this button runs the functions that store and render the scores
+initialsBtn.addEventListener("click", function (event) {
   event.preventDefault();
-// shows Home Page and Clear Scores buttons
+  // shows Home Page and Clear Scores buttons
   finalBtns.style.visibility = 'visible';
-var initialsStored = initialsField.value.trim();
+  var initialsStored = initialsField.value.trim();
 
-
+// if no initials are submitted, nothing happens
   if (initialsStored === "") {
     return;
   } else {
-// stores score and initials to local storage
+    // stores score and initials to local storage
     localStorage.setItem('initials', initialsStored);
     localStorage.setItem('final score', finalScore);
 
@@ -276,9 +303,8 @@ var initialsStored = initialsField.value.trim();
 
 
 
-
 // returns you to the home page if you wish, regardless of whether you wish to enter your initials and submit your score
-const reloadPage = () => {
+const reloadPage = function () {
   location.reload();
 }
 homePageBtn.addEventListener('click', reloadPage);
@@ -286,11 +312,21 @@ homePageBtn.addEventListener('click', reloadPage);
 
 
 clearScores.addEventListener("click", function () {
-  localStorage.clear(); 
-  scoresList.innerHTML = ""; 
+  localStorage.clear();
+  scoresList.innerHTML = "";
 });
 
 
-quizDataInit();
+
+// loads scores if there's previous scores
+function quizDataInit() {
+  var checkIfScoresDefined = JSON.parse(localStorage.getItem('scores'));
+  if (checkIfScoresDefined !== undefined) {
+    renderLatestScores();
+    console.log(`scores array defined`);
+  }
+}
+
+// quizDataInit();
 
 
